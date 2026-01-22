baa19a0e51b836f13711b01447c47931328564d0
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2026-01-22 09:47:38 -0400

    first commit

---
+++ fseabackend/src/main/java/com/finconecta/fseabackend/FseabackendApplication.java
@@ -0,0 +1,13 @@
+package com.finconecta.fseabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FseabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FseabackendApplication.class, args);
+	}
+
+}
---
+++ fseabackend/src/test/java/com/finconecta/fseabackend/FseabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finconecta.fseabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FseabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
